808a743b1bfa9a29848631722872c5d4ebf1e3bb
vahidtaleghani <bahareh.vahid.taleghani@gmail.com>
2022-04-27 09:58:49 +0200

    add error in add new TourVM

--- SWE2-TourPlanner/ViewModels/AddTourVM.cs
+++ SWE2-TourPlanner/ViewModels/AddTourVM.cs
@@ -1,5 +1,8 @@
 ﻿using SWE2_TourPlanner.BusinessLayer;
 using SWE2_TourPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -11,6 +14,9 @@ namespace SWE2_TourPlanner.ViewModels
         private string tourDescription;
         private string tourFrom;
         private string tourTo;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
 
         private ITourFactory tourFactory;
 
@@ -25,20 +31,101 @@ namespace SWE2_TourPlanner.ViewModels
 
         private void AddNewTour(object commandParameter)
         {
-            int id = this.tourFactory.GetLastTourId();
-            TourItem newTour = new TourItem(id, tourName, tourDescription, tourFrom, tourTo,tourName,0);
-            
-            //save to DB
-            this.tourFactory.CreateTourItem(newTour);
-            //save image to Folder
-            this.tourFactory.SaveRouteImageFromApi(TourFrom, TourTo, TourName);
+            if (!string.IsNullOrEmpty(TourName) && !string.IsNullOrEmpty(TourFrom) && !string.IsNullOrEmpty(TourTo) && !string.IsNullOrEmpty(TourDescription))
+            {
+                int id = this.tourFactory.GetLastTourId();
+                TourItem newTour = new TourItem(id, tourName, tourDescription, tourFrom, tourTo, tourName, 0);
 
-            MessageBox.Show("New Tour Successfully added.");
-            TourName = string.Empty;
-            TourFrom = string.Empty;
-            TourTo = string.Empty;
-            TourDescription = string.Empty;
+                //save to DB
+                this.tourFactory.CreateTourItem(newTour);
+                //save image to Folder
+                this.tourFactory.SaveRouteImageFromApi(TourFrom, TourTo, TourName);
 
+                MessageBox.Show("New Tour Successfully added.");
+                TourName = string.Empty;
+                TourFrom = string.Empty;
+                TourTo = string.Empty;
+                TourDescription = string.Empty;
+            }
+            else
+            {
+                CheckInputTourName();
+                CheckInputTourFrom();
+                CheckInputTourTo();
+                CheckInputTourDescription();
+            }
+
+        }
+
+
+        public bool CheckInputTourName()
+        {
+            ClearErrors(nameof(TourName));
+            if (string.IsNullOrWhiteSpace(TourName))
+            {
+                AddError(nameof(TourName), "Name cannot be empty.");
+                return false;
+            }
+            return true;
+        }
+
+        public bool CheckInputTourFrom()
+        {
+            ClearErrors(nameof(TourFrom));
+            if (string.IsNullOrWhiteSpace(TourFrom))
+            {
+                AddError(nameof(TourFrom), "Starting Point cannot be empty.");
+                return false;
+            }
+            return true;
+        }
+
+        public bool CheckInputTourTo()
+        {
+            ClearErrors(nameof(TourTo));
+            if (string.IsNullOrWhiteSpace(TourTo))
+            {
+                AddError(nameof(TourTo), "Destination cannot be empty.");
+                return false;
+            }
+            return true;
+        }
+
+        public bool CheckInputTourDescription()
+        {
+            ClearErrors(nameof(TourDescription));
+            if (string.IsNullOrWhiteSpace(TourDescription))
+            {
+                AddError(nameof(TourDescription), "Description cannot be empty.");
+                return false;
+            }
+            return true;
+        }
+
+        private void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void AddError(string propertyName, string error)
+        {
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+                _errorsByPropertyName[propertyName] = new List<string>();
+
+            if (!_errorsByPropertyName[propertyName].Contains(error))
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        private void ClearErrors(string propertyName)
+        {
+            if (_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
         }
 
 
